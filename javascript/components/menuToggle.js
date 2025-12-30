c7f22464b9a414cf4dd61065be86590c33a45378
Mayong Saputra <gilang46622@gmail.com>
2025-12-30 10:50:25 +0700

    style<header>: make it responsive for mobile and desktop

---
+++ javascript/components/menuToggle.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+});
