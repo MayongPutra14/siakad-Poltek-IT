b5c9b0375868349a435f650817e08d7ec9c2ef72
Mayong Saputra <gilang46622@gmail.com>
2026-01-10 18:00:54 +0700

    feature<theme>: create theme, add transition, preferences theme, and fix the toggle theme size

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -2,3 +2,4 @@ import "./components/menuToggle.js";
 import "./components/active-menu.js";
 import "./components/ipk-chart.js";
 import "./components/selected-menu.js";
+import "./components/theme.js";
