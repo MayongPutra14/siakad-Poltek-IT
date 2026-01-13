365c001a7a6a04bcbe77ca2e12c7270a14a679c4
Mayong Saputra <gilang46622@gmail.com>
2026-01-13 10:36:26 +0700

    Reset project structure and reinstall dependencies

---
+++ server/app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+
+/**
+ * ======================
+ * GLOBAL MIDDLEWARE
+ * ======================
+ */
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * ======================
+ * SESSION CONFIG
+ * ======================
+ */
+
+app.use(
+  session({
+    name: "siapit.sid",
+    secret: "siapit-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: "lax",
+    },
+  })
+);
+
+/**
+ * ======================
+ * ROUTES
+ * ======================
+ */
+
+const authRoutes = require("./routes/auth.routes.js");
+const mahasiswaRoutes = require("./routes/mahasiswa.routes.js");
+
+
+console.log("Auth Routes:", authRoutes);
+console.log("Mahasiswa Routes:", mahasiswaRoutes);
+
+app.use("/auth", authRoutes);
+app.use("/mahasiswa", mahasiswaRoutes);
+
+/**
+ * ======================
+ * EXPORT APP
+ * ======================
+ */
+
+module.exports = app;
